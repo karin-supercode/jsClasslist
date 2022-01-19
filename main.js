8afd9bd89206100d87fcea490fa8008f29a4c980
Supercoder <supercoder@AirvonSpercoder.local>
2022-01-19 14:36:57 +0100

    Übung Classlist

---
+++ main.js
@@ -0,0 +1,25 @@
+// Übung 1_1 classList 
+
+
+// let bucketListHeadline = document.getElementById("headline");
+// console.log(bucketListHeadline);
+// bucketListHeadline.classList.add("headlineKlasse");
+
+// let bucketListe = document.getElementById("liste");
+// console.log(bucketListe);
+// bucketListe.classList.add("listeKlasse");
+
+
+// Übung 2_1 classList 
+
+
+let styleHeadline = document.getElementById("headline")
+
+function changeHeadline() {
+  console.log(styleHeadline);
+  headline.classList.add("headlineKlasse")
+}
+function resetHeadline() {
+  console.log(styleHeadline);
+  headline.classList.remove("headlineKlasse")
+}
